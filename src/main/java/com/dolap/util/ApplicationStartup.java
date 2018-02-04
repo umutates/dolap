717f0f3d86c3d83a9a6766ed466df14c7f670713
umutates <umutates@DESKTOP-J93R766.mynet>
2018-02-05 00:14:25 +0300

    added applicationStartup

---
+++ src/main/java/com/dolap/util/ApplicationStartup.java
@@ -0,0 +1,22 @@
+package com.dolap.util;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationStartup 
+implements ApplicationListener<ApplicationReadyEvent> {
+
+  /**
+   * This event is executed as late as conceivably possible to indicate that 
+   * the application is ready to service requests.
+   */
+  @Override
+  public void onApplicationEvent(final ApplicationReadyEvent event) {
+
+   System.out.println("Umut");
+
+    return;
+  }
+}
