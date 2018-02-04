01aaf81f5b5fc6f6fba6ed21523cfd8559425f4f
umutates <umutates@DESKTOP-J93R766.mynet>
2018-02-05 00:46:39 +0300

    added admin user

--- src/main/java/com/dolap/util/ApplicationStartup.java
+++ src/main/java/com/dolap/util/ApplicationStartup.java
@@ -1,22 +1,33 @@
 package com.dolap.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import com.dolap.entity.User;
+import com.dolap.service.IUserService;
+import com.dolap.service.impl.UserService;
+
 @Component
 public class ApplicationStartup 
 implements ApplicationListener<ApplicationReadyEvent> {
-
+       @Autowired
+       IUserService userService;
   /**
    * This event is executed as late as conceivably possible to indicate that 
    * the application is ready to service requests.
    */
-  @Override
-  public void onApplicationEvent(final ApplicationReadyEvent event) {
+	@Override
+	public void onApplicationEvent(final ApplicationReadyEvent event) {
+		if (userService.findByEmail("admin@dolap.com") == null) {
+			User user = new User();
+			user.setEmail("admin@dolap.com");
+			user.setName("dolap");
+			user.setPassword("dolap");
+			user.setRole(Role.ADMIN.name());
+			userService.insert(user);
+		}
 
-   System.out.println("Umut");
-
-    return;
-  }
+	}
 }
