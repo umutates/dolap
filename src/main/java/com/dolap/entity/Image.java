0ebb8531f1698547316b9184244d65bbba5840e2
umutates <umutates@DESKTOP-J93R766.mynet>
2018-02-07 23:34:25 +0300

    bug fixed

--- src/main/java/com/dolap/entity/Image.java
+++ src/main/java/com/dolap/entity/Image.java
@@ -3,16 +3,11 @@ package com.dolap.entity;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Version;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 /**
--- src/main/java/com/dolap/util/ApplicationStartup.java
+++ src/main/java/com/dolap/util/ApplicationStartup.java
@@ -72,6 +72,54 @@ implements ApplicationListener<ApplicationReadyEvent> {
 		Product product2=new Product("Product2","Product2",new BigDecimal(100),images2,"NIKE",ProductType.BLAZERS.name());
 		productService.insert(product2);
 		
+		Set<Image> images3=new HashSet<Image>();
+		Image image5=new Image();
+		image5.setImagePath("images/home/product6.jpg");
+		image5.setFileName("product6.jpg");
+		Image image6=new Image();
+		image6.setImagePath("images/home/product1.jpg");
+		image6.setFileName("product1.jpg");
+		images3.add(image5);
+		images3.add(image6);
+		Product product3=new Product("Product3","Product3",new BigDecimal(100),images3,"NIKE",ProductType.TSHIRT.name());
+		productService.insert(product3);
+		
+		Set<Image> images4=new HashSet<Image>();
+		Image image7=new Image();
+		image7.setImagePath("images/home/product1.jpg");
+		image7.setFileName("product1.jpg");
+		Image image8=new Image();
+		image8.setImagePath("images/home/product2.jpg");
+		image8.setFileName("product2.jpg");
+		images4.add(image7);
+		images4.add(image8);
+		Product product4=new Product("Product4","Product4",new BigDecimal(100),images4,"NIKE",ProductType.SUNGLASS.name());
+		productService.insert(product4);
+		
+		Set<Image> images6=new HashSet<Image>();
+		Image image11=new Image();
+		image11.setImagePath("images/home/product4.jpg");
+		image11.setFileName("product4.jpg");
+		Image image12=new Image();
+		image12.setImagePath("images/home/product5.jpg");
+		image12.setFileName("product5.jpg");
+		images6.add(image11);
+		images6.add(image12);
+		Product product6=new Product("Product6","Product6",new BigDecimal(100),images6,"NIKE",ProductType.KIDS.name());
+		productService.insert(product6);
+		
+		Set<Image> images10=new HashSet<Image>();
+		Image image20=new Image();
+		image20.setImagePath("images/home/product2.jpg");
+		image20.setFileName("product5.jpg");
+		Image image21=new Image();
+		image21.setImagePath("images/home/product1.jpg");
+		image21.setFileName("product5.jpg");
+		images10.add(image20);
+		images10.add(image21);
+		Product product5=new Product("Product5","Product5",new BigDecimal(100),images10,"NIKE",ProductType.POLOSHIRT.name());
+		productService.insert(product5);
+		
 
 	}
 }
